794e10250f7aecc99e3f909b9d098d4b7610bd85
phineasphreak <riftpp@gmail.com>
2019-12-21 09:23:00 -0800

    Added batch file for compile python/pyinstaller

---
+++ batch/_pyinstaller_make_exe.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+TITLE Create exe With PYINSTALLER
+
+ECHO MAKE EXE
+pytinstaller.exe -w -F -i [icon_file] [python_file]
+
+ECHO DONE
+PAUSE
---
+++ batch/_python_compile_py.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+TITLE Compile Script with python3
+
+ECHO MAKE Compile python Script
+python.exe -m py_compile [python_file]
+
+ECHO DONE
+PAUSE
