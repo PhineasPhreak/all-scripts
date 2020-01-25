2b4e5e9976dd76e71aaffa2b6b06485f6608648a
pspk <32305432+PhineasPhreak@users.noreply.github.com>
2020-01-25 11:27:55 +0100

    Create GodMod_Win.bat

---
+++ batch/GodMod_Win.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+TITLE Shortcut GODMOD Windows
+REM Create the GODMOD shorcut on Windows 7, 8, 8.1, 10
+
+ECHO Create the GODMOD shorcut on Windows 7, 8, 8.1, 10
+MKDIR "GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}"
+ECHO Shortcut -- OK
+ECHO.
+
+PAUSE
