a006d4c0d100b098805886b313baadcba41f2113
pspk <32305432+PhineasPhreak@users.noreply.github.com>
2020-08-03 16:01:09 +0200

    Fix command name

--- bash/convert-heic.sh
+++ bash/convert-heic.sh
@@ -4,5 +4,5 @@
 # libheif1 (ISO/IEC 23008-12:2017 HEIF file format decoder - shared library)
 # libheif-examples (ISO/IEC 23008-12:2017 HEIF file format decoder - examples)
 
-for file in *.heic; do hieif-convert -q 100 $file ${file/%.heic/.jpg}; done
+for file in *.heic; do heif-convert -q 100 $file ${file/%.heic/.jpg}; done
 
