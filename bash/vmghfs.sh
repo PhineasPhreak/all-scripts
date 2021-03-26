53ff3b5e1107b4889247757d98906e746ecc2bc2
PhineasPhreak <riftpp@gmail.com>
2021-03-26 22:47:23 +1100

    add vmghfs.sh
    Mounting Shared Folders in a Linux Guest

---
+++ bash/vmghfs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# https://docs.vmware.com/en/VMware-Workstation-Pro/16.0/com.vmware.ws.using.doc/GUID-AB5C80FE-9B8A-4899-8186-3DB8201B1758.html
+#
+
+echo "Mounting Shared folder, on going..."
+sleep 0.5
+sudo mount -t fuse.vmhgfs-fuse -o allow_other,uid=1000,gid=1000 .host:/shared ~/shared/ || echo "Some error, open the bash file"
+sleep 0.5
+echo "'~/shared/' folder mounted, It's OKAY.
+
